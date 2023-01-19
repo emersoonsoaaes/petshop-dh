efbb7037e23fc1fd4a41e2a1a2332bcab24b3d93
Emerson Soares <emersoonsoaares@gmail.com>
2023-01-19 11:29:45 -0300

    Exercício de configurações de servidor e rotas

---
+++ src/app.js
@@ -0,0 +1,25 @@
+// Importações
+const express = require('express');
+const path = require('path');
+const homeRoutes = require('./routes/home');
+
+
+// Variaveis
+const app = express();
+const port = 4000;
+
+// Configuraçoes //Middlewares
+app.set('view engine', 'ejs');
+app.set("views", path.resolve("src", "views"));
+app.use(express.json());
+app.use(express.static(path.resolve("src", "public")));
+
+
+// Rotas
+app.use(homeRoutes)
+
+
+
+
+app.listen(port, () => console.log(`Servidor funcionando na porta ${port}`))
+// npm run dev -> nodemon | npm start -> node
